e105f4fa191eca85be97d4ec5a6395931c09c043
stkirk <shaanekirk@gmail.com>
2021-06-16 14:09:12 -0400

    set up npm, in Counter component imported state hook, created count and setCount pair with useState and replaced hardcoded font color with a ternary that will change color value to crimson is count is an odd number

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,12 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 --> import useState function */
 
 export default function Counter() {
   /* STEP 1 */
+  const [ count, setCount ] = useState(0); //create count and setCount pair and initialize count to 0
+  // console.log(count)
 
   const increment = () => {
     /* STEP 4 */
@@ -64,7 +66,7 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2 === 0 ? "royalblue" : "crimson", /* STEP 2 */
   };
 
   return (
